fa5af0258fefaa76fc4f1efdef1d3985e3d75a90
dcchuck <chuck.danielsson@gmail.com>
2016-12-02 00:31:35 -0500

    Gitignore, add part 2 of Q1 solution

--- day1/destination_finder.rb
+++ day1/destination_finder.rb
@@ -9,12 +9,16 @@ class DestinationFinder
   end
 
   def destination
-    find_destination(@starting_position, @instructions)
+    navigate_to_destination(@starting_position, @instructions)
   end
 
+  def first_intersection
+    visited_vertices_along_instructions(@starting_position, @instructions)
+  end
+    
   private
 
-  MOVE_AND_FACE = {
+  VECTOR_TRANSLATOR = {
     north: {
       right: [1, 0, :east],
       left: [-1, 0, :west]
@@ -33,15 +37,32 @@ class DestinationFinder
     }
   }
 
-  def find_destination starting_position, instructions
-    location = starting_position
+  def find_first_intersection
+
+  end
+  def visited_vertices_along_instructions position, instructions
+    visits = []
     instructions.each do |instruction|
-      adjustment_vector = MOVE_AND_FACE[location[2]][instruction[0]]
-      location[0] += adjustment_vector[0] * instruction[1]
-      location[1] += adjustment_vector[1] * instruction[1]
-      location[2] = adjustment_vector[2]
+      adjustment_vector = VECTOR_TRANSLATOR[position[2]][instruction[0]]
+      current_position = [position[0],position[1]]
+      instruction[1].times do |i|
+        visits << [current_position[0] + (adjustment_vector[0] * i), current_position[1] + (adjustment_vector[1] * i)]
+      end
+      position[0] += adjustment_vector[0] * instruction[1]
+      position[1] += adjustment_vector[1] * instruction[1]
+      position[2] = adjustment_vector[2]
     end
-    [location[0],location[1]]
+    visits.group_by{ |e| e }.select { |k, v| v.size > 1 }.map(&:first)[0]
+  end
+
+  def navigate_to_destination position, instructions
+    instructions.each do |instruction|
+      adjustment_vector = VECTOR_TRANSLATOR[position[2]][instruction[0]]
+      position[0] += adjustment_vector[0] * instruction[1]
+      position[1] += adjustment_vector[1] * instruction[1]
+      position[2] = adjustment_vector[2]
+    end
+    [position[0],position[1]]
   end
 end
 
@@ -54,6 +75,16 @@ describe DestinationFinder do
       destination_finder = DestinationFinder.new(instructions)
 
       expect(destination_finder.destination).to match_array([4,-2])
+    end
+  end
+
+  describe "#first_intersection" do
+    it "returns the first time you cross your own path when following directions" do
+      instruction_string = "R8, R4, R4, R8"
+      instructions = Instructions.new(instruction_string)
+      destination_finder = DestinationFinder.new(instructions)
+
+      expect(destination_finder.first_intersection).to match_array([4,0])
     end
   end
 end
